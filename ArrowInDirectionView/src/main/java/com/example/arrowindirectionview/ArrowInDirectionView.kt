fcb27ae8a2679a977e803c9d9aaff6cadecaa8e5
Anwesh43 <anweshthecool0@gmail.com>
2021-04-15 06:57:32 +0530

    adding Canvas extension methods

--- ArrowInDirectionView/src/main/java/com/example/arrowindirectionview/ArrowInDirectionView.kt
+++ ArrowInDirectionView/src/main/java/com/example/arrowindirectionview/ArrowInDirectionView.kt
@@ -29,3 +29,42 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawArrowInDirection(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val j : Float = 1f - 2 * (i % 2)
+    save()
+    translate(w / 2, h / 2)
+    scale(j, 1f)
+    save()
+    translate(-(w / 2) + size + (w - 2 * size) * sf3, 0f)
+    for (k in 0..2) {
+        save()
+        translate(size, 0f)
+        rotate((k - 1) * deg * sf2)
+        drawLine(
+            -(size * sf1 - (size * 0.5f * Math.abs(k - 1)) * sf2),
+            0f,
+            0f,
+            0f,
+            paint
+        )
+        restore()
+    }
+    restore()
+    restore()
+}
+
+fun Canvas.drawAIDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArrowInDirection(i, scale, w, h, paint)
+}
+
