99d8b1e7cb93eca7c3d244ac8c667f05c4b30097
Anwesh43 <anweshthecool0@gmail.com>
2021-04-15 07:12:59 +0530

    adding Renderer class

--- ArrowInDirectionView/src/main/java/com/example/arrowindirectionview/ArrowInDirectionView.kt
+++ ArrowInDirectionView/src/main/java/com/example/arrowindirectionview/ArrowInDirectionView.kt
@@ -194,4 +194,27 @@ class ArrowInDirectionView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArrowInDirectionView) {
+
+        private val aid : ArrowInDirection = ArrowInDirection(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            aid.draw(canvas, paint)
+            animator.animate {
+                aid.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            aid.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
