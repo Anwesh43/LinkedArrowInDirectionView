3e2b1e951e4f982ad2c1d83d6654f72e379a11c2
Anwesh43 <anweshthecool0@gmail.com>
2021-04-15 07:08:53 +0530

    adding AIDNode class

--- ArrowInDirectionView/src/main/java/com/example/arrowindirectionview/ArrowInDirectionView.kt
+++ ArrowInDirectionView/src/main/java/com/example/arrowindirectionview/ArrowInDirectionView.kt
@@ -130,4 +130,45 @@ class ArrowInDirectionView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AIDNode(var i : Int, val state : State = State()) {
+
+        private var next : AIDNode? = null
+        private var prev : AIDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AIDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAIDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AIDNode {
+            var curr : AIDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
