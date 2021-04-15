042faf53189969ea6bf83dfaa0c11188ccb56fc7
Anwesh43 <anweshthecool0@gmail.com>
2021-04-15 06:44:34 +0530

    adding constants and libraries

--- ArrowInDirectionView/src/main/java/com/example/arrowindirectionview/ArrowInDirectionView.kt
+++ ArrowInDirectionView/src/main/java/com/example/arrowindirectionview/ArrowInDirectionView.kt
@@ -1,2 +1,26 @@
 package com.example.arrowindirectionview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#01579B",
+    "#BF360C",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 6.7f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 45f
